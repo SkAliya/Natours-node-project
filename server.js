a46feef34fd7c9833543ffdbab12c7a3f4211932
Shaik Aliya <skaliya205@gmail.com>
2025-04-03 23:36:42 +0530

    installed eslint prettier plugins, modified eslintrc config file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -12,3 +13,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('listening for request......');
 });
+
+// const x = 20;
